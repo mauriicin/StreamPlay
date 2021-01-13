837e0123886b7e56e31ac1317f2bba8583826709
Mauricio Souza <mssoliveira1995@gmail.com>
2021-01-13 20:08:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+﻿<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">   
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+
+<meta name="language" content="pt-br">
+<meta property="og:site_name" content="StreamPlay">
+<meta property="og:url" content="http://www.streamplay.com.br/">
+<meta property="og:title" content="StreamPlay - Uma Novidade te espera!">
+<meta property="og:image" content="http://www.streamplay.com.br/fb-cover.png">
+<meta property="og:description" content="Ouça música online, Crie Playlists!">
+    
+<meta name="title" content="StreamPlay">
+<meta name="description" content="Uma Novidade te espera!">
+<meta name="keywords" content="radio, streamplay, rádio, brasil, música, pop, rock, rap, ouvir música, fm">
+
+<title>StreamPlay - Uma Novidade te espera!</title>
+<link rel="icon" href="media/images/favicon.png">
+<link href="media/css/style.css" rel="stylesheet" type="text/css">
+<link href="media/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<script>
+function irAllow() {
+    window.location.assign("http://www.allowdigital.com")
+}
+</script>
+</head>
+
+<body>
+    <div class="shadow">
+		<div class="barra"></div>
+    	<div class="allow right" onclick="irAllow()"></div>
+    	<div class="logo"></div>
+    	<div class="redes">
+        	<a href="//www.facebook.com/streamplayBrasil/" target="_blank"><div class="button"><i class="fb fa fa-facebook"></i></div></a>
+        	<a href="//twitter.com/streamplaybr" target="_blank"><div class="button"><i class="tt fa fa-twitter"></i></div></a>
+        </div>
+    </div>
+</body>
+</html>
